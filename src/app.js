fd9eb88e6571b481a322d9857a04d29d3a88e724
sanjai2208 <sanjaiboopathi2005@gmail.com>
2025-04-18 18:17:55 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Namste dashboard");
+app.get("/user",(req, res)=>{
+  res.send({firstName : "sanjai", city : "TKm"})
 });
+app.post("/user", (req, res)=>{
+  //saving data to database
+  res.send({firstName :"sanjai",
+    city : "tkm"
+  })
+  
+})
+app.delete("/user", (req, res)=>{
+  res.send("data deleted successfully ")
+  
+})
 
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello");
-});
 
-app.use("/test", (req, res) => {
-  res.send("Hello from server");
-});
+app.listen(2810, ()=>console.log("server is successfully running"))
 
-app.listen(3000, () => {
-  console.log("server is listening successfully on port 3000");
-});
