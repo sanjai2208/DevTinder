584d479005fee3df69b5d308e38c731d74ce74d2
sanjai2208 <sanjaiboopathi2005@gmail.com>
2025-04-24 21:01:43 +0530

    seventh commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,11 @@ const app = express();
 const User = require("./models/user");
 const { validateSignUpData, validateLogin } = require("./utils/validation");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser")
+const jwt = require("jsonwebtoken")
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.post("/signup", async (req, res) => {
   try {
@@ -30,12 +33,18 @@ app.post("/login" ,async (req, res) =>{
   try{
     validateLogin(req)
     const {emailId, password} = req.body;
+    
     const user =await User.findOne({emailId:emailId})
     if(!user){
       throw new Error ("Invalid Credential")
     }
     const isPasswordValid =await bcrypt.compare(password, user.password)
+
     if(isPasswordValid){
+
+      const token = jwt.sign({_id:user._id},"Dev@Tinder$2810")
+      
+      res.cookie("Token", token)
       res.send("Login successfully")
     }
     else{
@@ -45,7 +54,23 @@ app.post("/login" ,async (req, res) =>{
     res.status(400).send("Error : " + err.message);
   }
 })
+app.get("/profile", async (req, res) => {
+ try { const {Token} = req.cookies
+ if(!Token){
+  throw new Error("Invalid Token")
+ }
+  const decodedMsg = await jwt.verify(Token, "Dev@Tinder$2810")
 
+  const {_id} = decodedMsg
+  const user = await User.findOne({_id:_id})
+  if(!user){
+    throw new Error("User is not present")
+  }
+  res.send(user)}
+  catch (err) {
+    res.status(400).send("Error : " + err.message);
+  }
+})
 app.get("/user", async (req, res) => {
   try {
     const user = await User.findOne({ emailId: req.body.emailId });
